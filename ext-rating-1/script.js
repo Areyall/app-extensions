994f8d7f6a7ab14e93f8003752e87fd1a8c16de5
Areyall <anhetec@gmail.com>
2022-07-18 21:19:02 +0300

    Connect JS average

--- ext-rating-1/script.js
+++ ext-rating-1/script.js
@@ -1,1 +1,21 @@
 const reviewBarContainer = document.querySelector('.review-bar');
+const averageReviewElem = document.querySelector('[data-avarage-review]');
+
+const REVIEWS = {
+  5: 120,
+  4: 40,
+  3: 20,
+  2: 0,
+  1: 0,
+};
+const totalReviews = Object.values(REVIEWS).reduce((sum, value) => {
+  return sum + value;
+}, 0);
+const averageReview =
+  Object.entries(REVIEWS).reduce((sum, [value, quantity]) => {
+    return sum + value * quantity;
+  }, 0) / totalReviews;
+
+averageReviewElem.textContent = Math.round(averageReview * 10) / 10;
+
+console.log('hello')
