32862ea8092849343b5487fdde32ff702b0b55e6
Areyall <anhetec@gmail.com>
2022-08-16 23:03:49 +0300

    JS

--- CDTimer/script.js
+++ CDTimer/script.js
@@ -1,7 +1,22 @@
 const startNumber = 5;
 const flipCard = document.querySelector('.flip-card');
+const topHalf = flipCard.querySelector('top');
+const bottomHalf = flipCard.querySelector('bottom');
 
 [...flipCard.children].forEach((child) => {
   child.textContent = startNumber;
 });
+
+flipCard.dataset.currentNumber = startNumber;
+flipCard.dataset.nextNumber = startNumber - 1;
+
+
+flipCard.addEventListener('animationstart', (e) => {
+    topHalf.textContent = startNumber - 1
+});
+flipCard.addEventListener('animationend', (e) => {
+    topHalf.textContent = startNumber - 1
+});
+
+
 flipCard.classList.add('flip');
