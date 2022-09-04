7ab1c977a64f9a647898327eb38d2bf043b9abac
Areyall <anhetec@gmail.com>
2022-09-04 21:04:09 +0300

    Setup aEvLi

--- CCR/script.js
+++ CCR/script.js
@@ -1,1 +1,9 @@
+const sliders = document.querySelectorAll('input[type="range"]');
+const billingInput = document.getElementById('bill');
 
+sliders.forEach((e) => e.addEventListener('input', calculateTip));
+billingInput.addEventListener('change', calculateTip);
+
+function calculateTip() {
+    
+}
