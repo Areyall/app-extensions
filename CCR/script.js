2c38f887bab20cac7c1f4b14222257961a8dd06f
Areyall <anhetec@gmail.com>
2022-09-05 21:29:59 +0300

    Loging inputs

--- CCR/script.js
+++ CCR/script.js
@@ -1,10 +1,15 @@
 const sliders = document.querySelectorAll('[data-range]');
 const billingInput = document.getElementById('bill');
-console.log(sliders)
+// console.log(sliders);
 
 sliders.forEach((e) => e.addEventListener('input', calculateTip));
 billingInput.addEventListener('change', calculateTip);
 
 function calculateTip() {
+  let bill = parseFloat(billingInput.value);
+  let tipPercent = document.getElementById('tip').value;
+  let noOfPeople = document.getElementById('no-of-people').value;
+//   console.log(bill, tipPercent, noOfPeople);
+
 
 }
